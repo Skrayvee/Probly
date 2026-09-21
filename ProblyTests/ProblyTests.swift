50735a461aae8aa61008f2465420d3e9795263e0
Skrayvee <sefik.gulmetov@gmail.com>
2026-09-21 15:08:44 +0300

    Initial Commit

---
+++ ProblyTests/ProblyTests.swift
@@ -0,0 +1,19 @@
+//
+//  ProblyTests.swift
+//  ProblyTests
+//
+//  Created by Skr Red on 21.09.2026.
+//
+
+import Testing
+@testable import Probly
+
+struct ProblyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
